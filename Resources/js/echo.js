5e8784874252609dbdf6c49f4780a81e12fabec8
marco xot <marco.sottana@gmail.com>
2022-11-30 18:08:15 +0100

    up

--- Resources/js/echo.js
+++ Resources/js/echo.js
@@ -1,8 +1,8 @@
-//import Echo from 'laravel-echo';
+import Echo from 'laravel-echo';
 
-//window.Pusher = require('pusher-js');
-
-/*window.Echo = new Echo({
+window.Pusher = require('pusher-js');
+/*
+window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
     wsHost: process.env.MIX_PUSHER_HOST,
@@ -14,23 +14,19 @@
     enableLogging: true,
     disableStats: true,
     enabledTransports: ['ws', 'wss'],
-});*/
-
-import Echo from "laravel-echo"
-
-window.Pusher = require('pusher-js');
+});
+*/
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
-    wsHost: process.env.MIX_LARAVEL_WEBSOCKET_ADDRESS,
+    wsHost: 'staging.mediamonitor.it',
+    wsPath: '/socket',
     wsPort: process.env.MIX_PUSHER_PORT,
     wssPort: process.env.MIX_PUSHER_PORT,
     forceTLS: false,
-    disableStats: false,
+    encrypted: true,
+    enableLogging: true,
+    disableStats: true,
+    enabledTransports: ['ws', 'wss'],
 });
-
-console.log({
-    key: process.env.MIX_PUSHER_APP_KEY,
-    wsHost: process.env.MIX_LARAVEL_WEBSOCKET_ADDRESS
-})
