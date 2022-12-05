9a87a163a213d552aa3a32779f94e0b153923968
marco xot <marco.sottana@gmail.com>
2022-12-05 19:13:11 +0100

    up

--- Resources/js/echo.js
+++ Resources/js/echo.js
@@ -1,32 +1,19 @@
 import Echo from 'laravel-echo';
 
 window.Pusher = require('pusher-js');
-/*
-window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: process.env.MIX_PUSHER_APP_KEY,
-    wsHost: process.env.MIX_PUSHER_HOST,
-    //wsPath: '/socket',
-    wsPort: process.env.MIX_PUSHER_PORT,
-    wssPort: process.env.MIX_PUSHER_PORT,
-    forceTLS: false,
-    encrypted: true,
-    enableLogging: true,
-    disableStats: true,
-    enabledTransports: ['ws', 'wss'],
-});
-*/
+
+console.log('5');
+console.log(process.env.MIX_PUSHER_APP_KEY);
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
-    wsHost: 'socket.mediamonitor.it',
-    //wsPath: '/socket',
+    wsHost: process.env.MIX_PUSHER_HOST,
     wsPort: process.env.MIX_PUSHER_PORT,
     wssPort: process.env.MIX_PUSHER_PORT,
-    forceTLS: false,
-    encrypted: true,
-    enableLogging: true,
+   // forceTLS: false,
+   // encrypted: true,
+   // enableLogging: true,
     disableStats: true,
-    enabledTransports: ['ws', 'wss'],
+   // enabledTransports: ['ws', 'wss'],
 });
