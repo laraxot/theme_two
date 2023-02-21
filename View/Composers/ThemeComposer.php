a4783246da08c8c2657798217075e90706d04c5e
marco xot <marco.sottana@gmail.com>
2023-02-21 17:22:08 +0100

    up

--- View/Composers/ThemeComposer.php
+++ View/Composers/ThemeComposer.php
@@ -4,5 +4,21 @@ declare(strict_types=1);
 
 namespace Themes\Two\View\Composers;
 
-class ThemeComposer {
+use Illuminate\Support\Collection;
+use Illuminate\View\View;
+use Modules\Xot\View\Composers\XotBaseComposer;
+
+class ThemeComposer extends XotBaseComposer {
+    /**
+     * Bind data to the view.
+     *
+     * @return void
+     */
+    public function compose(View $view) {
+        $view->with('_theme', $this);
+    }
+
+    public function getFavouritesPresses(): Collection {
+        return collect([]);
+    }
 }
